ea627a07b2bdfcffb299da773291eb7ff8a9adb4
Cristian686504 <cristian.renz@estudiantes.utec.edu.uy>
2026-04-09 00:01:02 -0300

    se quitó un import que no se utilizaba

--- src/test/java/org/prueba/questions/ElCatalogo.java
+++ src/test/java/org/prueba/questions/ElCatalogo.java
@@ -2,7 +2,6 @@ package org.prueba.questions;
 
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Question;
-import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
 import org.prueba.ui.PaginaPropiedades;
 
 public class ElCatalogo implements Question<Boolean> {
